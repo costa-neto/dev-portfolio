35bccade4b2ba11066900182de74b504e130768c
Neto Costa <neto1809costa@gmail.com>
2023-11-09 12:06:29 -0300

    feat: add dark mode resources

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="container mx-auto max-w-2xl">
       <header class="flex  justify-between items-center mt-5">
-        <div>
-          <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">Neto Costa</NuxtLink>
+        <div class="flex items-center space-x-12">
+          <div>
+            <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">Neto Costa</NuxtLink>
+          </div>
+
+          <Menu />
         </div>
-        <Menu />
+
+        <ClientOnly >
+          <ColorModeSelector />
+        </ClientOnly>
       </header>
         
         <main class="p-2 mt-10">
@@ -33,5 +40,9 @@ useHead({
 <style>
 body {
   font-family: 'Roboto';
+}
+
+body {
+  @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300
 }
 </style>
