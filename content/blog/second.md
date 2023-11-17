9542925e6f97326dc1c051ba3dd3bb82abd5d68e
Neto Costa <neto1809costa@gmail.com>
2023-11-17 20:59:27 -0300

    add code syntax highlighting and fetching content

--- content/blog/second.md
+++
@@ -1,1 +0,0 @@
-Second blog post.
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,3 +1,17 @@
 <template>
-    <div>All blogs posts list!</div>
+    <section>
+        <ul>
+            <li v-for="post in posts" :key="post._path">
+                <NuxtLink :to="post._path"> {{ post.title }}</NuxtLink>
+            </li>
+        </ul>
+    </section>
 </template>
+
+<script setup lang="ts">
+const { data: posts, pending, error, refresh } = await useAsyncData(
+    'blog-list',
+    () => queryContent('/blog').only(['_path', 'title']).find()
+);
+
+</script>
