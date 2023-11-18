911870b0e6f29e21b8d0db4d2a120737ab7ff21d
Neto Costa <neto1809costa@gmail.com>
2023-11-18 11:19:52 -0300

    error handling

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+    <NuxtLayout>
+        <section>
+            <h1 class="text-4xl mb-4">
+                Oooppss.. {{ error.statusCode }}
+            </h1>
+            <h2 class="text-xl text-gray-400 dark:text-gray-400">
+                {{ error.statusMessage }}
+            </h2>
+        </section>
+    </NuxtLayout>
+</template>
+
+<script setup>
+defineProps({ error: Object })
+</script>
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,21 +1,28 @@
 <template>
     <article class="prose dark:prose-invert prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300 max-w-none">
-        <ContentDoc v-slot="{ doc }">
-            <div class="grid grid-cols-6 gap-16">
-                <div :class="{'col-span-4': doc.toc, 'col-span-6': !doc.toc}">
-                    <ContentRenderer :value="doc" />
+        <ContentDoc >
+            <template #not-found>
+                <h1>Document not found</h1>
+                <p>This blog post couldn't be found.</p>
+            </template>
+            <template v-slot="{ doc }">
+                <div class="grid grid-cols-6 gap-16">
+                    <div :class="{'col-span-4': doc.toc, 'col-span-6': !doc.toc}">
+                        <ContentRenderer :value="doc" />
+                    </div>
+                    <div class="col-span-2 not-prose" v-if="doc.toc">
+                        <aside class="sticky top-8">
+                            <div class="font-semibold mb-2">
+                                Table
+                            </div>
+                            <nav>
+                                <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
+                            </nav>
+                        </aside>
+                    </div>
                 </div>
-                <div class="col-span-2 not-prose" v-if="doc.toc">
-                    <aside class="sticky top-8">
-                        <div class="font-semibold mb-2">
-                            Table
-                        </div>
-                        <nav>
-                            <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
-                        </nav>
-                    </aside>
-                </div>
-            </div>
+            </template>
+            
         </ContentDoc>
     </article>
 </template>
